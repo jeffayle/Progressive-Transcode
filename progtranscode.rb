8d748942bd801691278d0624390c562ebbc88e6f
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-01-04 18:15:22 -0500

    Move one second of samples to file

--- progtranscode.rb
+++ progtranscode.rb
@@ -4,6 +4,14 @@
 `flac -d -o output.raw --force-raw-format --endian=little \
 --sign=signed input.flac`
 
+i = 0
 while File.size 'output.raw' > 0
-    
+    fin = File.open 'output.raw', 'rb'
+    fout = File.open "storage/#{i}", 'wb'
+    fout.write(fin.read(44100*2*2)) #Move one second of audio
+            #(44100 samples/second, 2 bytes/sample, 2 channels)
+    fin.close
+    fout.close
+
+    i += 1
 end
