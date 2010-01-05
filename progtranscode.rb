e73b56275dfd6c93a2dc836b4f7425e86361c198
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-01-04 19:03:31 -0500

    Does all the encoding

--- progtranscode.rb
+++ progtranscode.rb
@@ -1,17 +1,27 @@
 #!/usr/bin/ruby
 
 #Initial decode to raw
-`flac -d -o output.raw --force-raw-format --endian=little \
+`flac -d -o storage/output.raw --force-raw-format --endian=little \
 --sign=signed input.flac`
+fout = File.open 'storage/final.raw', 'wb'
 
-i = 0
-while File.size 'output.raw' > 0
-    fin = File.open 'output.raw', 'rb'
-    fout = File.open "storage/#{i}", 'wb'
+while File.size('storage/output.raw') > 0
+    fin = File.open 'storage/output.raw', 'rb'
     fout.write(fin.read(44100*2*2)) #Move one second of audio
             #(44100 samples/second, 2 bytes/sample, 2 channels)
+    remainder = fin.read
     fin.close
-    fout.close
+    #Now write it back, with the first second clipped off
+    outTemp = File.open 'storage/output.raw', 'wb'
+    outTemp.write remainder
+    outTemp.close
 
-    i += 1
+    puts "**** %d bytes left"%(remainder.length)
+
+    #Encode mp3
+    `lame -r --bitwidth 16 --signed --little-endian -V 2 \
+    storage/output.raw storage/output.mp3`
+    #Then decode it
+    `lame --decode  --bitwidth 16 --signed --little-endian \
+    storage/output.mp3 storage/output.raw`
 end
